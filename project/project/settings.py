941fe07d1fd7eede23d1a7bf1f61fbca009f5db4
yusufananikhsan09 <yusufanani82@gmail.com>
2023-10-22 21:47:59 +0800

    Step 3 - Configure Static & API

--- project/project/settings.py
+++ project/project/settings.py
@@ -130,6 +130,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "assets/static"
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "assets/media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
